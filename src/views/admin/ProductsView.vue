cc6f33595121c0c9ab722bbe90945d29c915dfad
Pochinsky <tomas.guttman@sansano.usm.cl>
2024-07-04 10:43:31 -0400

    🎉 project initializated

---
+++ src/views/admin/ProductsView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Link from "@/components/Link.vue";
+</script>
+
+<template>
+  <Link to="add-product">Agregar Producto</Link>
+  <h1 class="text-4xl font-black my-10">Productos</h1>
+</template>
---
+++ src/views/admin/SalesView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1 class="text-4xl font-black my-10">Resumen de Ventas</h1>
+</template>
